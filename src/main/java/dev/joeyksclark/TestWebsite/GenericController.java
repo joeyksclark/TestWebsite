49bcb4fec6499872b959bfb29fb8d025060766f6
Joey Clark <coolrockerjoey@gmail.com>
2021-09-11 16:14:09 -0700

    Add /login

--- src/main/java/dev/joeyksclark/TestWebsite/GenericController.java
+++ src/main/java/dev/joeyksclark/TestWebsite/GenericController.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class GenericController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
