be2f375e78aecb519407fa546f0944e5932e8b34
Joey Clark <coolrockerjoey@gmail.com>
2021-09-11 14:53:58 -0700

    Create Maven project

---
+++ src/main/java/dev/joeyksclark/TestWebsite/TestWebsiteApplication.java
@@ -0,0 +1,13 @@
+package dev.joeyksclark.TestWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joeyksclark/TestWebsite/TestWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joeyksclark.TestWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
